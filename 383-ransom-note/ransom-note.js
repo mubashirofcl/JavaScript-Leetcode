1cac013229534f30c2dc6b9c8619b4f9aac9da60
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2025-08-25 16:41:54 +0530

    Time: 21 ms (37.25%) | Memory: 61 MB (15.41%) - LeetSync

---
+++ 383-ransom-note/ransom-note.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+    const count = {};
+
+    for (let char of magazine) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of ransomNote) {
+        if (!count[char]) {
+            return false;
+        }
+        count[char]--; 
+    }
+
+    return true;
+};
