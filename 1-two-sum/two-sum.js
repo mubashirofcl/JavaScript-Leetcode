553bd202945df87fdb57e35fb30062d27571fa64
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2025-10-15 09:11:56 +0530

    Time: 1 ms (87.65%) | Memory: 58.1 MB (13.08%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -1,13 +1,15 @@
-var twoSum = function(nums, target) {
-    const map = new Map(); 
-
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const numMap = new Map();
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i]; 
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
         }
-        map.set(nums[i], i); 
+        numMap.set(nums[i], i);
     }
-
-    return []; 
 };
