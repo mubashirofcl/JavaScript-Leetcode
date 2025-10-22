1c737ca71767647c0a0bbbc44f2a6e838d5f167e
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2025-10-22 12:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (20.19%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function (celsius) {
+    let Kelvin = celsius + 273.15;
+    let Fahrenheit = celsius * 1.80 + 32.00;
+
+    ans = [Kelvin, Fahrenheit];
+    return ans
+};
