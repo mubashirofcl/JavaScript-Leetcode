271d88d51474778bcfa004807463f51e831db718
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2025-11-12 13:40:23 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (24.16%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function (a, b) {
+    let count = 0;
+    for (let i = 1; i <= a; i++) {
+        if (a % i == 0 && b % i == 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
