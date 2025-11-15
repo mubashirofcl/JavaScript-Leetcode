74d5d28b57e550eaaed1306b169aaa23be0f06ae
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2025-11-15 09:09:39 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (17.63%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function (n) {
+    const res = [];
+    for (let i = 1; i <= Math.floor(n / 2); i++) {
+        res.push(i, -i);
+    }
+    if (n % 2 === 1) res.push(0);
+    return res;
+};
