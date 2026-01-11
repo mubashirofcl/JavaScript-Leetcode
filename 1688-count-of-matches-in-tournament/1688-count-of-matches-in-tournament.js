26e424c40bd9c8a18815661b3caf4cc5fa5fbaab
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2026-01-11 16:06:34 +0530

    Time: 45 ms (56.76%), Space: 53.9 MB (65.95%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -2,13 +2,6 @@
  * @param {number} n
  * @return {number}
  */
-var numberOfMatches = function (n) {
-    let count = 0;
-    let rev = 0;
-    while (n > 1) {
-        rev = Math.floor(n / 2);
-        count += rev;
-        n = n - rev;
-    }
-    return count;
+var numberOfMatches = function(n) {
+    return n - 1;
 };
