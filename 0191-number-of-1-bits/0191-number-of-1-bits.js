eb83322cddcec10fcec19ad49dbe9f9a14274a1b
MUBASHIR PALAPADIYAN <78842905+mubashirofcl@users.noreply.github.com>
2026-06-29 20:44:57 +0530

    Time: 0 ms (100%), Space: 55.7 MB (13.09%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+    let setBitCount = 0;
+    while (n !== 0) {
+        n &= (n - 1);
+        setBitCount++;
+    }
+    return setBitCount;
+};
